93fb84b274bc9529661708787d65a4732f35588b
Elias Mina <hukitan@hotmail.com>
2024-12-04 18:21:30 -0600

    se hace un coso para probar los ciclos for que se usaran

--- pittsburg.R
+++ pittsburg.R
@@ -14,7 +14,14 @@ abs(as.integer(pittsburg[14,6] - pittsburg[14,8])/3600)
 
 if (pittsburg[10,10] == "Menos de una vez a la semana") {
     print("pan")
-} 
+} else if (pittsburg[10,10]) == "Ninguna vez en el último mes"{
+   print("aceituna")
+}
 
 #resultados (?)
 res <- tibble()
+t <- (0)
+
+for (p in 0:nrow(pittsburg)) {
+    t[p] <- pittsburg[p,10]
+}
