a49722e19cb7ec04bb0da4f3b1bcb701e5cdba7d
Elias Mina <hukitan@hotmail.com>
2024-12-06 15:48:05 -0600

    se avanza en la correccion de los valores

--- pittsburg.R
+++ pittsburg.R
@@ -4,7 +4,7 @@ library(tidyverse)
 source('get_Data.R', chdir = TRUE)
 
 
-tbl_colnames <- c("id","sexo", "edad", "1","2","3","4", "5a", "5b","5c","5d","5e", "5f","5g","5h","5i", "5j","6","7","8","9" )
+tbl_colnames <- c("id","sexo", "edad", "1","2","3","4", "5a", "5b","5c","5d","5e", "5f","5g","5h","5i","Sum_aj","p_sum","6","7","8","sum" )
 proc_fct <- tbl_colnames %>% purrr::map_dfc(setNames, object = list(logical())) # hace una tible vacia con nombres en tbl_colnames y la nombra res
 rm(tbl_colnames)
 
@@ -33,18 +33,70 @@ for (p in 1:nrow(pittsburg)) {
     proc_fct[p,5] <- pittsburg[p,7]
 }
 
-#hay que corregir unas cosas 3.
+#3. hay que verificar que los datos si sean almacenados bien
 for (p in 1:nrow(pittsburg)) {
     proc_fct[p,6] <- pittsburg[p,8]
 }
 
-#hay que corregir unas cosas 4.
+# 4.
 for (p in 1:nrow(pittsburg)) {
     proc_fct[p,7] <- pittsburg[p,9]
 }
 
 #5a-5j 
 orig <- 10:18
+for (p in 1:nrow(pittsburg)) {
+    proc_fct[p,17] <- 0
+    for (o in orig) {
+        r <- o-2
+        if (pittsburg[p,o] == "Ninguna vez en el último mes") {
+            proc_fct[p,r] <- 0
+        } else if  (pittsburg[p,o] == "Menos de una vez a la semana") {
+            proc_fct[p,r] <- 1
+        }else if  (pittsburg[p,o] == "Una o dos veces a la semana") {
+            proc_fct[p,r] <- 2
+        }else if  (pittsburg[p,o] == "Tres o más veces a la semana") {
+            proc_fct[p,r] <- 3
+        } else {
+            proc_fct[p,r] <- 999
+        }
+        proc_fct[p,17] <- proc_fct[p,17] + proc_fct[p,r]
+    }
+}
+rm(p,o,r,orig)
+
+#caluclo del puntaje de 5
+for (p in 1:nrow(proc_fct)) {
+    if (proc_fct[p,17] == 0) {
+        proc_fct[p,18] <- 0
+    } else if  (proc_fct[p,17] <= 9) {
+        proc_fct[p,18] <- 1
+    }else if  (proc_fct[p,17] <= 18) {
+        proc_fct[p,18] <- 2
+    }else if  (proc_fct[p,17] <= 27) {
+        proc_fct[p,18] <- 3
+    } else {
+        proc_fct[p,18] <- 999
+    }
+}
+rm(p)
+
+for (p in 1:nrow(pittsburg)) {
+    if (pittsburg[p,21] == "Ninguna vez en el último mes") {
+        proc_fct[p,19] <- 0
+    } else if  (pittsburg[p,21] == "Menos de una vez a la semana") {
+        proc_fct[p,19] <- 1
+    }else if  (pittsburg[p,21] == "Una o dos veces a la semana") {
+        proc_fct[p,19] <- 2
+    }else if  (pittsburg[p,21] == "Tres o más veces a la semana") {
+        proc_fct[p,19] <- 3
+    } else {
+        proc_fct[p,19] <- 9
+    }
+    rm(p)
+}
+
+orig <- 21:22
 for (p in 1:nrow(pittsburg)) {
     for (o in orig) {
         r <- o-2
@@ -61,4 +113,4 @@ for (p in 1:nrow(pittsburg)) {
         }
     }
 }
-rm(p,o,r,orig,dest)
+rm(p,o,r,orig)
