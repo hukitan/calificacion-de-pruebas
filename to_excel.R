a12e8bfa06d83b22df2ebee95e093cd2b8a865fd
Elias Mina <hukitan@hotmail.com>
2024-12-21 12:52:59 -0600

    Refactor to_excel.R: simplificación de la escritura de datos y mejora en la gestión de hojas de trabajo

--- to_excel.R
+++ to_excel.R
@@ -1,44 +1,35 @@
-if (require(openxlsx)) {
-    library(openxlsx)
-} else {
+if (!require(openxlsx)) {
     install.packages("openxlsx", dep = TRUE)
     library(openxlsx)
 }
 
+# Crear estilo de encabezado
 hs1 <- createStyle(
     fgFill = "#cc71e2", halign = "CENTER",
     textDecoration = "bold", border = "Bottom"
 )
 
+# Crear libro de trabajo
 wb <- createWorkbook("Alastor")
 
-# creamos las hojas
-addWorksheet(wb, "Resultados")
-addWorksheet(wb, "Cuestionario")
+# Función para configurar anchos de columna y escribir datos
+escribir_datos <- function(hoja, datos, cols) {
+    addWorksheet(wb, hoja)
+    setColWidths(wb, hoja, cols = cols, widths = "auto")
+    writeData(wb, hoja, datos, borders = "surrounding", headerStyle = hs1, startRow = 1)
+}
 
-setColWidths(wb, "Cuestionario", cols = 1:4, widths = "auto")
-writeData(wb, "Cuestionario", pittsburg[-5],
-    borders = "surrounding",
-    headerStyle = hs1, startRow = 1
-)
+# Escribir datos en las hojas
+escribir_datos("Cuestionario", pittsburg[-5], 1:4)
+escribir_datos("Resultados", proc_fct, 1:nrow(proc_fct))
 
-
-setColWidths(wb, "Resultados", cols = 1:nrow(proc_fct), widths = "auto")
-writeData(wb, "Resultados", proc_fct, borders = "surrounding", headerStyle = hs1, startRow = 1)
-
-
-
+# Guardar y abrir el libro de trabajo según el sistema operativo
+saveWorkbook(wb, "Pittsburg.xlsx", overwrite = TRUE, returnValue = FALSE)
 switch(Sys.info()[["sysname"]],
-    Windows = {
-    saveWorkbook(wb, "Pittsburg.xlsx", overwrite = TRUE, returnValue = FALSE)
-    shell("Pittsburg.xlsx")
-    },
-    Linux = {
-    saveWorkbook(wb, "Pittsburg.xlsx", overwrite = TRUE, returnValue = FALSE)
-    system2(command = "open", args = "Pittsburg.xlsx")
-    },
-    Darwin = {
-        print("I'm a Mac.")
-    }
+    Windows = shell("Pittsburg.xlsx"),
+    Linux = system2(command = "open", args = "Pittsburg.xlsx"),
+    Darwin = system2(command = "open", args = "Pittsburg.xlsx")
 )
+
+# Eliminar variables temporales
 rm(hs1, wb)
